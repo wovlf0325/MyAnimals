adceb5f62cf41dc8ba871d4fe7fe55db5c36270b
wovlf0325 <zerg_kid@naver.com>
2020-02-13 19:28:21 +0900

    로그인 회원가입 틀

---
+++ MyAnimals/src/com/member/dao/MemberDaoImpl.java
@@ -0,0 +1,76 @@
+package com.member.dao;
+
+import java.util.List;
+
+import com.member.db.SqlMapConfig;
+import com.member.dto.MemberDto;
+
+public class MemberDaoImpl extends SqlMapConfig implements MemberDao {
+
+	@Override
+	public List<MemberDto> selectList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MemberDto> selectUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MemberDto> selectCenter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateRole(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDto selectOne(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateInfo(MemberDto dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDto login(String id, String pw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int regist(MemberDto dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDto findId(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MemberDto findPw(String id, String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
